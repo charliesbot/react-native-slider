e31ba64fb6fa837a83ca516d96000403e07a7a46
Charlie L <charliesbot@gmail.com>
2020-05-03 12:00:33 -0700

    migrate to typescript

---
+++ src/constants/animationConfig.ts
@@ -0,0 +1,13 @@
+import { Easing } from "react-native";
+
+export const DEFAULT_ANIMATION_CONFIGS = {
+  spring: {
+    friction: 7,
+    tension: 100,
+  },
+  timing: {
+    duration: 150,
+    easing: Easing.inOut(Easing.ease),
+    delay: 0,
+  },
+};
